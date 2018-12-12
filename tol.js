8f8285b0ec557ea7b6b82610f188b6063ea909a9
Lea Hughes <lahughesmusic@gmail.com>
2018-12-11 19:20:55 -0600

    add files

--- tol.js
+++ tol.js
@@ -1,1 +1,20 @@
-s;
+document.getElementById("bird").onclick = function() {
+  location.href = "./bird.html";
+};
+
+document.getElementById("followbird").onclick = function() {
+  location.href = "./catbird.html";
+};
+document.getElementById("explanation").onclick = function() {
+  location.href = "./explanation.html";
+};
+document.getElementById("bookIt").onclick = function() {
+  location.href = "./road.html";
+};
+document.getElementById("catbird").onclick = function() {
+  location.href = "./catbird.html";
+};
+
+document.getElementById("catbird").onclick = function() {
+  location.href = "./catbird.html";
+};
